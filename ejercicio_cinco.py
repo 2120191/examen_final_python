fe9d6d9e354944e3a486303a451f5f9f7e066da5
2120191 <jorgeluisromeromejia4@gmail.com>
2024-07-17 10:23:08 -0500

    solucion:ejercicio uno

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -5,24 +5,4 @@ entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
 
-nombre="jose"
-apellido="alvarez"
-edad=30
-curso="apsti"
-periodo="III"
-def persona(nom,ape,edad,cur,peri):
-    return {
-    #   "nombre":nom
-    #   "apellido":ape
-    #   "edad":edad
-    #   "curso":cur
-    #   "periodo":peri
-    }
-    return dict(
-        nombre=nom,
-        apellido=ape,
-        edad=edad,
-        curso=cur,
-        periodo=peri,
-    )
-print(persona(nombre,apellido,edad,curso,periodo))
+
--- ejercicio_dos.py
+++ ejercicio_dos.py
@@ -2,15 +2,3 @@
 crear una funcion que haciendo uso del metodo filter me filtre los numeros primos de una lista pasada por parametros
 """
 
-def es_primo(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-def filtrar_primos(lista):
-    return list(filter(es_primo, lista))
-numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-primos = filtrar_primos(numeros)
-print('Números primos en la lista:', primos)
