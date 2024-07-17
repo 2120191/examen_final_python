81a6fc73e33d722c18fd09c4cbe90a5991190588
2120191 <jorgeluisromeromejia4@gmail.com>
2024-07-17 10:27:56 -0500

    solucion:ejercicio dos

--- ejercicio_dos.py
+++ ejercicio_dos.py
@@ -2,3 +2,23 @@
 crear una funcion que haciendo uso del metodo filter me filtre los numeros primos de una lista pasada por parametros
 """
 
+
+def es_primo(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def filtrar_primos(lista):
+    return list(filter(es_primo, lista))
+
+# Lista de ejemplo
+numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Filtrar números primos de la lista
+primos = filtrar_primos(numeros)
+
+print('Números primos en la lista:', primos)
