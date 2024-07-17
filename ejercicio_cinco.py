dd75a5fe809e2b1d82f12bf3c3cd9d49870e1ae6
2120191 <jorgeluisromeromejia4@gmail.com>
2024-07-17 10:52:27 -0500

    solucion:ejercicio cinco

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -5,24 +5,10 @@ entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
 
-nombre="jose"
-apellido="alvarez"
-edad=30
-curso="apsti"
-periodo="III"
-def persona(nom,ape,edad,cur,peri):
-    return {
-    #   "nombre":nom
-    #   "apellido":ape
-    #   "edad":edad
-    #   "curso":cur
-    #   "periodo":peri
-    }
-    return dict(
-        nombre=nom,
-        apellido=ape,
-        edad=edad,
-        curso=cur,
-        periodo=peri,
-    )
-print(persona(nombre,apellido,edad,curso,periodo))
+def datos_alumno(nombre, apellido, edad, programa_estudio, semestre):
+    return {'nombre': nombre, 'apellido': apellido, 'edad': edad, 'programa_estudio': programa_estudio, 'semestre': semestre}
+
+# Datos de ejemplo
+alumno = datos_alumno('Jose', 'Alvarez', '30', 'APSTI', 'III')
+
+print(alumno)
